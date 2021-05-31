0ffb23120912970cf9d69acf58d23abaaaaf76e1
Patricia <pattricianunes@gmail.com>
2021-05-31 08:28:06 +0200

    Updates

---
+++ precache-manifest.31ac9f76b6f663a4181bde4537732ad1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab8c7a317f20ef34b6cd93231e6c1c8",
+    "url": "/jwtlogin-reactjs/index.html"
+  },
+  {
+    "revision": "c6bc6c63fa463ca3f6ef",
+    "url": "/jwtlogin-reactjs/static/js/2.4bf0fee9.chunk.js"
+  },
+  {
+    "revision": "938c8e8a8ffb7161e407",
+    "url": "/jwtlogin-reactjs/static/js/main.5ad02a3a.chunk.js"
+  },
+  {
+    "revision": "4386f8cce98169d4db0a",
+    "url": "/jwtlogin-reactjs/static/js/runtime~main.91f2787d.js"
+  },
+  {
+    "revision": "81f75af8e133fc2ba46076d9d40460d4",
+    "url": "/jwtlogin-reactjs/static/media/eye.81f75af8.ico"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/jwtlogin-reactjs/precache-manifest.31ac9f76b6f663a4181bde4537732ad1.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jwtlogin-reactjs/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
